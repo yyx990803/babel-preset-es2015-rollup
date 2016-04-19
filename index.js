d557b07a77aa7082a8ca92178262b48e0e1e49dd
Evan You <yyx990803@gmail.com>
2016-04-18 22:33:25 -0400

    edits

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var modify = require('modify-babel-preset');
 module.exports = modify('es2015', {
 	// remove commonjs transform
 	'transform-es2015-modules-commonjs': false,
-
+  // remove typeof symbol
+  'transform-es2015-typeof-symbol': false,
 	// add external helpers
 	'external-helpers': true
 });
