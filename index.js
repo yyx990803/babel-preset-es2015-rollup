aea8fecf3103fbf92ce3add706eba7d794fc04ee
Evan You <yyx990803@gmail.com>
2016-05-08 00:11:26 -0400

    use simple class transform

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ module.exports = modify('es2015', {
   // remove typeof symbol
   'transform-es2015-typeof-symbol': false,
 	// add external helpers
-	'external-helpers': true
+	'external-helpers': true,
+  // use simple class transform (loose + without callcheck)
+  'transform-es2015-classes': false,
+  'transform-es2015-classes-simple': true
 });
