5b6879ad03e346e06c644d258840d69fe34dc738
Evan You <yyx990803@gmail.com>
2016-05-11 14:05:18 -0400

    format code style

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-var modify = require('modify-babel-preset');
-
-module.exports = modify('es2015', {
-	// remove commonjs transform
-	'transform-es2015-modules-commonjs': false,
+module.exports = require('modify-babel-preset')('es2015', {
+  // remove commonjs transform
+  'transform-es2015-modules-commonjs': false,
   // remove typeof symbol
   'transform-es2015-typeof-symbol': false,
-	// add external helpers
-	'external-helpers': true,
+  // add external helpers
+  'external-helpers': true,
   // use simple class transform (loose + without callcheck)
   'transform-es2015-classes': false,
   'transform-es2015-classes-simple': true
-});
+})
